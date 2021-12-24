f226de9cd7e463a744bf5745dcf65c4931158c5a
zhouHaoZhuang <88356815+zhouHaoZhuang@users.noreply.github.com>
2021-12-24 10:13:08 +0800

    格式化代码

--- src/store/modules/pay.js
+++ src/store/modules/pay.js
@@ -4,20 +4,23 @@ const income = {
   namespaced: true,
   actions: {
     // 获取充值记录
-    getVal({ commit, state},data ) {
+    getVal({
+      commit,
+      state
+    }, data) {
       return request({
-				url: "/pay/onlineTopUp",
-				method: "post",
-				data,
-			})
+        url: "/pay/onlineTopUp",
+        method: "post",
+        data,
+      })
     },
-    // getOne({ commit, state }, id) {
-    //   console.log(state);
-    //   return request({
-    //     url: `/tcOrder/${id}`,
-    //     method: "get"
-    //   });
-    // }
+    getBalance({ commit, state }, id) {
+      console.log(state);
+      return request({
+        url: `/tcOrder`,
+        method: "get"
+      });
+    }
   },
 };
 
