8e66ec83afe372af9bc8b804b2198ac068c8f690
韩启航 <1878806686@qq.com>
2022-02-17 18:44:02 +0800

    图片上传组件

--- src/utils/index.js
+++ src/utils/index.js
@@ -20,14 +20,34 @@ export function base64ToFile(base64, filename) {
 }
 // 获取并返回图片base64字符串对象
 export function getBase64Str(base64, type) {
-  const fileContents = base64;
-  const index = type.indexOf("/");
-  const fileSuffix = type.substring(index + 1);
+  const arr = base64.split(",");
+  const fileContents = arr[1];
+  const fileSuffix = arr[0];
   return {
     fileContents,
     fileSuffix
   };
 }
+
+// 将网络地址图片转换为base64
+export const imgUrlToBase64 = (imgUrl) => {
+  let image = new Image();
+  image.crossOrigin = "anonymous"; //解决跨域问题
+  image.src = imgUrl;
+  return new Promise((resolve) => {
+    image.onload = function () {
+      //image.onload为异步加载
+      let canvas = document.createElement("canvas");
+      canvas.width = image.width;
+      canvas.height = image.height;
+      let context = canvas.getContext("2d");
+      context.drawImage(image, 0, 0, image.width, image.height);
+      //这里的base64Url就是base64类型
+      //使用toDataUrl将图片转换成jpeg的格式,不要把图片压缩成png，因为压缩成png后base64的字符串可能比不转换前的长！
+      resolve(canvas.toDataURL("image/jpeg", 1));
+    };
+  });
+};
 // 请求列表时前端要替后端做适配，并且有点多，封装请求列表时带有搜索的接口
 // request: 调用vuex的actions名   listQuery: 传递给后端的参数
 // 设置需要处理为精确查询的名单
