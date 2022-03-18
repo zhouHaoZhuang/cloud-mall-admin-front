7d8d1832ca41bfc7f10472b21503b4b3a3950079
zhouHaoZhuang <88356815+zhouHaoZhuang@users.noreply.github.com>
2022-03-18 18:53:42 +0800

    增加企业认证

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -130,6 +130,37 @@ const user = {
         method: "get",
       });
     },
+     // 获取企业实名认证信息
+     getEnterpriseRealNameInfo({ commit, state }, data) {
+      return request({
+        url: `/ccCompanyInfo/oneCompanyInfo`,
+        method: "get"
+      });
+    },
+    // 获取企业实名认证信息(营业执照)
+    businessImgData({ commit, state }, data) {
+      return request({
+        url: `/ccCompanyInfo/businessOcr`,
+        method: "post",
+        data
+      });
+    },
+    // 获取企业实名认证信息(身份证 正反面)
+    legalPersonData({ commit, state }, data) {
+      return request({
+        url: `/ccCompanyInfo/idCardOcr`,
+        method: "post",
+        data
+      });
+    },
+    //上传企业实名认证信息
+    uploadEnterpriseRealName({ commit, state }, data) {
+      return request({
+        url: `/ccCompanyInfo/legalPerson`,
+        method: "post",
+        data
+      });
+    },
     // 获取认证状态
     getCertification({ commit, state }, flowId) {
       return request({
--- src/views/user/setting/verified/enterprise.vue
+++ src/views/user/setting/verified/enterprise.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="enterprise">
     <h2>
       <a-button style="margin-right: 20px" @click="$router.back()">
         <a-icon type="left" />
@@ -16,12 +16,16 @@
       <a-form-model-item label=" " :colon="false">
         <h3>
           <img
-            v-if="false"
-            src="@/assets/img/realName/enterpriseRealNameError.png"
+            v-show="realNameStatus === 1"
+            src="@/assets/img/realName/ralNameError.png"
             alt=""
           />
-          <img v-if="false" src="@/assets/img/realName/attestOk.png" alt="" />
-          当前账号尚未进行企业实名认证
+          <img
+            v-show="realNameStatus === 2"
+            src="@/assets/img/realName/realNameOk.png"
+            alt=""
+          />
+          {{ realNameStatusText[realNameStatus] }}
         </h3>
       </a-form-model-item>
       <a-form-model-item :colon="false">
@@ -40,17 +44,29 @@
               "
             />
           </a-form-model-item>
-          <div class="example-img">
+          <div class="example-img" v-show="realNameStatus !== 2">
             <img width="90px" src="@/assets/img/realName/business.png" alt="" />
             <p>示例图</p>
           </div>
         </div>
       </a-form-model-item>
       <a-form-model-item label="企业名称">
-        <span>通过上传营业执照获取</span>
+        <a-input
+          v-if="form.businessName !== '' && realNameStatus !== 2"
+          v-model="form.businessName"
+        >
+        </a-input>
+        <span v-else-if="realNameStatus === 2">{{ form.businessName }}</span>
+        <span v-else>通过上传营业执照获取</span>
       </a-form-model-item>
       <a-form-model-item label="社会信用代码">
-        <span>通过上传营业执照获取</span>
+        <a-input
+          v-if="form.certNo !== '' && realNameStatus !== 2"
+          v-model="form.certNo"
+        >
+        </a-input>
+        <span v-else-if="realNameStatus === 2">{{ form.certNo }}</span>
+        <span v-else>通过上传营业执照获取</span>
       </a-form-model-item>
       <a-form-model-item :colon="false">
         <span slot="label" class="form-title">法人信息</span>
@@ -81,11 +97,11 @@
               title="上传(国徽)"
             />
           </a-form-model-item>
-          <div class="example-img">
+          <div class="example-img" v-show="realNameStatus !== 2">
             <img width="90px" src="@/assets/img/realName/avatar.png" alt="" />
             <p>示例图</p>
           </div>
-          <div class="example-img">
+          <div class="example-img" v-show="realNameStatus !== 2">
             <img
               width="90px"
               src="@/assets/img/realName/nationalEmblem.png"
@@ -96,13 +112,29 @@
         </div>
       </a-form-model-item>
       <a-form-model-item label="法人姓名">
-        <span>通过上传身份证获取</span>
+        <a-input
+          v-if="form.name !== '' && realNameStatus !== 2"
+          v-model="form.name"
+        ></a-input>
+        <span v-else-if="realNameStatus === 2">{{ form.name }}</span>
+        <span v-else>通过上传身份证获取</span>
       </a-form-model-item>
       <a-form-model-item label="身份证号码">
-        <span>通过上传身份证获取</span>
+        <a-input
+          v-if="form.idNo !== '' && realNameStatus !== 2"
+          v-model="form.idNo"
+        ></a-input>
+        <span v-else-if="realNameStatus === 2">{{ form.idNo }}</span>
+        <span v-else>通过上传身份证获取</span>
       </a-form-model-item>
-      <a-form-model-item :wrapper-col="{ span: 14, offset: 8 }">
-        <a-button type="primary" @click="onSubmit"> {{ btnTiele }} </a-button>
+      <a-form-model-item :wrapper-col="{ span: 14, offset: 9 }">
+        <a-button
+          v-show="realNameStatus !== 2"
+          type="primary"
+          @click="onSubmit"
+        >
+          提交审核
+        </a-button>
       </a-form-model-item>
     </a-form-model>
   </div>
@@ -110,18 +142,23 @@
 
 <script>
 import Upload from "@/components/Upload/index";
+
 export default {
   components: {
     Upload
   },
   data() {
     return {
-      labelCol: { span: 8 },
-      wrapperCol: { span: 14 },
+      labelCol: { span: 9 },
+      wrapperCol: { span: 10 },
       form: {
         businessImg: "",
         avatarImg: "",
-        emblemImg: ""
+        emblemImg: "",
+        businessName: "",
+        certNo: "",
+        name: "",
+        idNo: ""
       },
       rules: {
         businessImg: [
@@ -146,48 +183,124 @@ export default {
           }
         ]
       },
-      btnTiele: "提交审核",
-      realNameStatus: {
-        1: "当前账号尚未进行企业实名认证",
-        2: "企业实名认证失败，请核实后重新提交认证",
-        3: "企业实名认证已完成"
+      realNameData: {},
+      realNameStatus: 0,
+      realNameStatusText: {
+        0: "当前账号尚未进行企业实名认证",
+        1: "企业实名认证失败，请核实后重新提交认证",
+        2: "企业实名认证已完成"
       }
     };
   },
   created() {
-    // this.getEnterpriseRealNameInfo();
+    this.$nextTick(() => {
+      this.resetForm();
+      this.getEnterpriseRealNameInfo();
+    });
   },
   methods: {
     // 获取企业实名认证的数据信息
     getEnterpriseRealNameInfo() {
       this.$store.dispatch("user/getEnterpriseRealNameInfo").then((res) => {
+        if (!res || !res.data) {
+          this.realNameStatus = 0;
+          return;
+        }
+        this.realNameStatus = 2;
+        this.form.businessImg = res.data.businessImg;
+        this.form.businessName = res.data.companyName;
+        this.form.avatarImg = res.data.infoImg;
+        this.form.emblemImg = res.data.emblemImg;
+        this.form.name = res.data.legalName;
+        this.form.idNo = res.data.legalNo;
+        this.form.certNo = res.data.companyCode;
+      });
+    },
+    // 上传营业执照图片
+    businessImgData(url) {
+      this.$store
+        .dispatch("user/businessImgData", { licenseImg: url })
+        .then((res) => {
+          // console.log(res);
+          this.form.businessName = res.data.name;
+          this.form.certNo = res.data.certNo;
+          this.realNameData = { ...this.realNameData, ...res.data };
+          this.realNameData.businessName = res.data.name;
+          // this.form = res.data;
+        });
+    },
+    // 上传身份证图片
+    legalPersonData() {
+      let data = {
+        infoImg: this.form.avatarImg,
+        emblemImg: this.form.emblemImg
+      };
+      this.$store.dispatch("user/legalPersonData", data).then((res) => {
         console.log(res);
-        this.form = res.data;
+        this.form.name = res.data.name;
+        this.form.idNo = res.data.idNo;
+        this.realNameData = { ...this.realNameData, ...res.data };
+        this.realNameData.legalName = res.data.name;
+        this.realNameData.legalNo = res.data.idNo;
       });
     },
     // 提交实名认证审核
     onSubmit() {
       this.$refs.ruleForm.validate((valid) => {
         if (valid) {
+          this.realNameData.businessName = this.form.businessName;
+          this.realNameData.certNo = this.form.certNo;
+          this.realNameData.legalName = this.form.name;
+          this.realNameData.legalNo = this.form.idNo;
+          this.realNameData.licenseImg = this.form.businessImg;
+          this.realNameData.infoImg = this.form.avatarImg;
+          this.realNameData.emblemImg = this.form.emblemImg;
           this.$store
-            .dispatch("user/uploadEnterpriseRealName", this.form)
+            .dispatch("user/uploadEnterpriseRealName", this.realNameData)
             .then((res) => {
               this.$message.success("提交成功");
               console.log(res, "认证结果");
+              if (res.data === true) {
+                this.realNameStatus = 2;
+              }
+              if (res.data === false) {
+                this.realNameStatus = 1;
+              }
             });
         }
       });
     },
     imgList(urlList, firstImageUrl, imgType) {
       console.log(urlList, firstImageUrl, imgType);
-      this.form[imgType] = urlList;
+      this.form[imgType] = urlList.toString();
+      if (imgType === "businessImg" && firstImageUrl) {
+        this.businessImgData(firstImageUrl);
+      }
+      if (this.form.businessImg === "") {
+        this.form.businessName = "";
+        this.form.certNo = "";
+      }
+      if (this.form.avatarImg.length > 0 && this.form.emblemImg.length > 0) {
+        this.legalPersonData();
+      }
+      if (
+        this.form.avatarImg.length === "" ||
+        this.form.emblemImg.length === ""
+      ) {
+        this.form.name = "";
+        this.form.idNo = "";
+      }
     },
     resetForm() {
       this.$refs.ruleForm.resetFields();
       this.form = {
         businessImg: "",
         avatarImg: "",
-        emblemImg: ""
+        emblemImg: "",
+        businessName: "",
+        certNo: "",
+        name: "",
+        idNo: ""
       };
     }
   }
@@ -195,14 +308,17 @@ export default {
 </script>
 
 <style lang="less" scoped>
-h2 {
-  display: flex;
-  align-items: center;
+.enterprise {
+  width: 100%;
+  background-color: #fff;
+  overflow: hidden;
+  margin: 0 10px;
 }
 h3 {
   font-size: 18px;
   font-weight: bold;
   color: #333;
+  margin-bottom: 0;
 }
 .form-title {
   font-size: 16px;
