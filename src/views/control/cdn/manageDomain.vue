1672547e041eaf61e5fd2d0eabaf588cc29471a1
韩启航 <1878806686@qq.com>
2022-03-18 16:28:01 +0800

    init

---
+++ src/views/control/cdn/manageDomain.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="cdn--container"></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  computed: {},
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {}
+};
+</script>
+
+<style lang="less" scoped>
+.cdn--container {
+}
+</style>
