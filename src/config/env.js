5ee074e41eab5d8ef94c31cc78408e327525dd16
zhouHaoZhuang <88356815+zhouHaoZhuang@users.noreply.github.com>
2022-03-23 20:02:48 +0800

    修改域名

--- src/config/env.js
+++ src/config/env.js
@@ -25,7 +25,7 @@ function getCloudMallUrl() {
 }
 // 请求头携带domain参数
 const domains = {
-  local: "t940857.site.sailayun.com"
+  local: "t410796.site.sailayun.com"
 };
 export default {
   BASE_URL: baseUrl[process.env.VUE_APP_ENV],
