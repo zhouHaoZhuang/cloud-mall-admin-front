78641c724ed2ff4c2b4c856906b3ec27eca6b854
李钰 <yu.li@yfmail.cn>
2022-03-23 21:05:34 +0800

    提示修改

--- src/components/withdraw/addApply.vue
+++ src/components/withdraw/addApply.vue
@@ -22,7 +22,7 @@
         </a-form-model-item>
 
         <a-form-model-item label="银行卡号" prop="accountNo">
-          <a-input v-model="form.accountNo" placeholder="请输入银行卡号" />
+          <a-input v-model="form.accountNo" v-number-evolution placeholder="请输入银行卡号" />
           <span class="tigs">请确保符合银行卡号规则</span>
         </a-form-model-item>
         <a-form-model-item label="银行卡绑定人" prop="receiverName">
@@ -37,7 +37,7 @@
             v-model="form.dealAmount"
             ref="dealAmount"
             placeholder="请输入需要提现的金额"
-            @change="toValidate"
+            v-number-evolution="{ value: 2, }"
           />
           <span class="tigs">请确保符合当前账号下余额充足</span>
         </a-form-model-item>
@@ -103,23 +103,12 @@ export default {
     }
   },
   data() {
-    const validateAccount = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请输入银行卡号"));
-      } else {
-        if (!this.codeReg.test(value)) {
-          callback(new Error("银行卡号不符合编号规则"));
-        }
-        callback();
-      }
-    };
     return {
       newTitle: "",
       confirmLoading: false,
       labelCol: { span: 6 },
       wrapperCol: { span: 16 },
       balance: 0,
-      codeReg: /[^\d]/g, //银行卡校验正则
       form: {
         status: 2,
         accountName: "",
@@ -139,21 +128,15 @@ export default {
         accountNo: [
           {
             required: true,
-            validator: validateAccount,
+             message: "银行卡号未填写",
             trigger: ["blur", "change"]
           }
         ],
         dealAmount: [
           {
             required: true,
+             message: "余额未填写",
             trigger: ["blur", "change"],
-            validator: (rule, value, callback) => {
-              if (!value) {
-                callback(new Error("请输入提现余额"));
-              } else {
-                callback();
-              }
-            }
           }
         ],
         receiverName: [
@@ -245,27 +228,6 @@ export default {
         }
       });
     },
-    // 校验提现余额
-    toValidate(e) {
-      //如果用户第一位输入的是小数点，则重置输入框内容
-      if (e.target.value != "" && e.target.value.substr(0, 1) == ".") {
-        e.target.value = "";
-      }
-      e.target.value = e.target.value.replace(/^0*(0\.|[1-9])/, "$1"); //粘贴不生效
-      e.target.value = e.target.value.replace(/[^\d.]/g, ""); //清除“数字”和“.”以外的字符
-      e.target.value = e.target.value.replace(/\.{2,}/g, "."); //只保留第一个. 清除多余的
-      e.target.value = e.target.value
-        .replace(".", "$#$")
-        .replace(/\./g, "")
-        .replace("$#$", ".");
-      e.target.value = e.target.value.toString().match(/^\d+(?:\.\d{0,2})?/); //只能输入两个小数
-      if (e.target.value.indexOf(".") < 0 && e.target.value != "") {
-        //以上已经过滤，此处控制的是如果没有小数点，首位不能为类似于 01、02的金额
-        if (e.target.value.substr(0, 1) == "0" && e.target.value.length == 2) {
-          e.target.value = e.target.value.substr(1, e.target.value.length);
-        }
-      }
-    },
     getDashboardData() {
       this.$store.dispatch("dashboard/getBalanceAndCoupon").then((res) => {
         const newData = {
