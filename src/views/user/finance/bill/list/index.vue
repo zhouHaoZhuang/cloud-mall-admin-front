d10e34561bbf9d9381fdae5b93b6176d9f44aed6
zhouHaoZhuang <88356815+zhouHaoZhuang@users.noreply.github.com>
2022-03-28 19:57:58 +0800

    修改路由跳转

--- src/views/user/finance/bill/list/index.vue
+++ src/views/user/finance/bill/list/index.vue
@@ -72,7 +72,7 @@
               </a-descriptions-item>
               <a-descriptions-item label=""> </a-descriptions-item>
               <a-descriptions-item label="">
-                <a @click="$router.push('/user/finance/bill/billmanage')">
+                <a @click="$router.push('/user/finance/bill/managebill')">
                   管理发票信息→
                 </a>
               </a-descriptions-item>
