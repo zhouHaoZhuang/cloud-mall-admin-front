172d7c46c9f4873e36efb166c9851c34685c82cd
zhouHaoZhuang <88356815+zhouHaoZhuang@users.noreply.github.com>
2022-03-29 11:30:38 +0800

    修改字段

--- src/views/user/finance/bill/info/list.vue
+++ src/views/user/finance/bill/info/list.vue
@@ -25,13 +25,13 @@
           ￥{{ data.invoiceAmount }}
         </a-descriptions-item>
         <a-descriptions-item label="申请时间">
-          {{ data.invoiceInfo.createTimeShow }}
+          {{ data.createTimeShow }}
         </a-descriptions-item>
         <a-descriptions-item label="反馈时间">
-          {{ data.feedbackTimeShow }}
+          {{ data.rejectTimeShow }}
         </a-descriptions-item>
         <a-descriptions-item label="反馈说明">
-          {{ data.refundFeedbackRemark }}
+          {{ data.rejectRemark }}
         </a-descriptions-item>
       </a-descriptions>
       <a-descriptions style="margin: 20px 0" title="收件人信息">
@@ -49,7 +49,7 @@
           {{ data.addressInfo.address }}
         </a-descriptions-item>
         <a-descriptions-item label="物流单号">
-          {{ data.expressDelivery }}
+          {{ data.businessExpressDelivery }}
         </a-descriptions-item>
       </a-descriptions>
     </div>
